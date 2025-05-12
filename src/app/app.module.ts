83898e662c679cd216429226ca0cb0c30504c02f
Radhika <radhika@vivejaitservices.com>
2025-05-12 14:29:27 +0530

    api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -6,6 +6,10 @@ import { UserFormComponent } from './components/user-form/user-form.component';
 import { FormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { userReducer } from './store/user/user.reducer';
+import { HttpClientModule } from '@angular/common/http';
+import { EffectsModule } from '@ngrx/effects';
+
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -15,8 +19,11 @@ import { userReducer } from './store/user/user.reducer';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule,
-    StoreModule.forRoot({ user: userReducer })
+    StoreModule.forRoot({ user: userReducer }),
+    // EffectsModule.forRoot([UserEffects]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/store/user/user.actions.ts
+++ src/app/store/user/user.actions.ts
@@ -1,7 +1,25 @@
 import { createAction, props } from '@ngrx/store';
 import { User } from './user.model';
 
-export const addUser = createAction('[User] Add', props<{ user: User }>());
-export const deleteUser = createAction('[User] Delete', props<{ id: number }>());
-export const selectUser = createAction('[User] Select', props<{ id: number }>());
-export const updateUser = createAction('[User] Update', props<{ user: User }>());
+// Load Users
+export const loadUsers = createAction('[User] Load Users');
+export const loadUsersSuccess = createAction('[User] Load Users Success', props<{ users: User[] }>());
+export const loadUsersFailure = createAction('[User] Load Users Failure', props<{ error: string }>());
+
+// Add User
+export const addUser = createAction('[User] Add User', props<{ user: User }>());
+export const addUserSuccess = createAction('[User] Add User Success', props<{ user: User }>());
+export const addUserFailure = createAction('[User] Add User Failure', props<{ error: string }>());
+
+// Update User
+export const updateUser = createAction('[User] Update User', props<{ user: User }>());
+export const updateUserSuccess = createAction('[User] Update User Success', props<{ user: User }>());
+export const updateUserFailure = createAction('[User] Update User Failure', props<{ error: string }>());
+
+// Delete User
+export const deleteUser = createAction('[User] Delete User', props<{ id: number }>());
+export const deleteUserSuccess = createAction('[User] Delete User Success', props<{ id: number }>());
+export const deleteUserFailure = createAction('[User] Delete User Failure', props<{ error: string }>());
+
+// Select User
+export const selectUser = createAction('[User] Select User', props<{ user: User }>());
